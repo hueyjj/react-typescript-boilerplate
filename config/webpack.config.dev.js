9f89b0a9bf71545cee741af683dd04c4f743154b
jasperjeng@gmail.com <jasperjeng@gmail.com>
2019-10-06 21:58:09 -0700

    Convert tabs to spaces

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -9,36 +9,34 @@ const pages = require("./pages");
 const paths = require("./paths");
 
 const config = {
-	mode: "development",
-	watch: true,
-	watchOptions: {
-		poll: 1000,
-		ignored: /node_modules/,
-	},
-	stats: "normal",
-	devtool: "inline-source-map",
+  mode: "development",
+  watchOptions: {
+    ignored: /node_modules/,
+  },
+  stats: "normal",
+  devtool: "inline-source-map",
   entry: pages.entries,
   output: {
-		path: paths.appBuild,
-		filename: "static/js/[name].[contenthash:8].js",
-		futureEmitAssets: true,
-		chunkFilename: "static/js/[name].[contenthash:8].chunk.js",
+    path: paths.appBuild,
+    filename: "static/js/[name].[contenthash:8].js",
+    futureEmitAssets: true,
+    chunkFilename: "static/js/[name].[contenthash:8].chunk.js",
   },
   optimization: {
-		minimize: false,
-		splitChunks: {
-			chunks: "all",
-			name: false,
-		},
-		runtimeChunk: {
-			name: entrypoint => `runtime-${entrypoint.name}`,
-		},
-	},
+    minimize: false,
+    splitChunks: {
+      chunks: "all",
+      name: false,
+    },
+    runtimeChunk: {
+      name: entrypoint => `runtime-${entrypoint.name}`,
+    },
+  },
   resolve: {
-		modules: [
-			paths.appSrc,
-			"node_modules",
-		],
+    modules: [
+      paths.appSrc,
+      "node_modules",
+    ],
     extensions: [
       ".js",
       ".jsx",
@@ -48,78 +46,78 @@ const config = {
   },
   module: {
     rules: [
-			{
-				oneOf: [
-					{
-						test: /\.(js|jsx)$/,
-						use: "babel-loader",
-						exclude: /node_modules/
-					},
-					{
-						test: /\.(ts|tsx)?$/,
-						loader: "ts-loader",
-						exclude: /node_modules/
-					},
-					{
-						test: /\.scss$/,
-						use: [
-							MiniCssExtractPlugin.loader,
-							"css-loader",
-							"sass-loader"
-						]
-					},
-					{
-						test: /\.png$/,
-						use: [
-							{
-								loader: "url-loader",
-								options: {
-									mimetype: "image/png"
-								}
-							}
-						]
-					},
-					{
-						loader: "file-loader",
-						exclude: [/\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/],
-						options: {
-							name: "static/media/[name].[hash:8].[ext]",
-						},
-					},
-				]
-			},
-		]
-	},
-	plugins: [
-		new CleanWebpackPlugin(),
-		new CopyWebpackPlugin([
-			{ from: "public" },
-		]),
-		...Object.keys(pages.entries).map(entryName =>
-			new HtmlWebpackPlugin({
-				inject: true,
-				chunks: [entryName],
-				filename: `${entryName}.html`,
-				template: `./templates/${entryName}.html`,
-				favicon: "./public/favicon.ico",
-				minify: {
-					removeComments: true,
-					collapseWhitespace: true,
-					removeRedundantAttributes: true,
-					useShortDoctype: true,
-					removeEmptyAttributes: true,
-					removeStyleLinkTypeAttributes: true,
-					keepClosingSlash: true,
-					minifyJS: true,
-					minifyCSS: true,
-					minifyURLs: true,
-				},
-			})
-		),
-		new MiniCssExtractPlugin({
-			filename: "static/css/[name].[contenthash:8].css",
-			chunkFilename: "static/css/[name].[contenthash:8].chunk.css",
-		}),
+      {
+        oneOf: [
+          {
+            test: /\.(js|jsx)$/,
+            use: "babel-loader",
+            exclude: /node_modules/
+          },
+          {
+            test: /\.(ts|tsx)?$/,
+            loader: "ts-loader",
+            exclude: /node_modules/
+          },
+          {
+            test: /\.scss$/,
+            use: [
+              MiniCssExtractPlugin.loader,
+              "css-loader",
+              "sass-loader"
+            ]
+          },
+          {
+            test: /\.png$/,
+            use: [
+              {
+                loader: "url-loader",
+                options: {
+                  mimetype: "image/png"
+                }
+              }
+            ]
+          },
+          {
+            loader: "file-loader",
+            exclude: [/\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/],
+            options: {
+              name: "static/media/[name].[hash:8].[ext]",
+            },
+          },
+        ]
+      },
+    ]
+  },
+  plugins: [
+    // new CleanWebpackPlugin(),
+    new CopyWebpackPlugin([
+      { from: "public" },
+    ]),
+    ...Object.keys(pages.entries).map(entryName =>
+      new HtmlWebpackPlugin({
+        inject: true,
+        chunks: [entryName],
+        filename: `${entryName}.html`,
+        template: `./templates/${entryName}.html`,
+        favicon: "./public/favicon.ico",
+        minify: {
+          removeComments: true,
+          collapseWhitespace: true,
+          removeRedundantAttributes: true,
+          useShortDoctype: true,
+          removeEmptyAttributes: true,
+          removeStyleLinkTypeAttributes: true,
+          keepClosingSlash: true,
+          minifyJS: true,
+          minifyCSS: true,
+          minifyURLs: true,
+        },
+      })
+    ),
+    new MiniCssExtractPlugin({
+      filename: "static/css/[name].[contenthash:8].css",
+      chunkFilename: "static/css/[name].[contenthash:8].chunk.css",
+    }),
   ],
 }
 
--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -11,56 +11,56 @@ const pages = require("./pages");
 const paths = require("./paths");
 
 const config = {
-	mode: "production",
+  mode: "production",
   entry: pages.entries,
   output: {
-		path: paths.appBuild,
-		filename: "static/js/[name].[contenthash:8].js",
-		futureEmitAssets: true,
-		chunkFilename: "static/js/[name].[contenthash:8].chunk.js",
+    path: paths.appBuild,
+    filename: "static/js/[name].[contenthash:8].js",
+    futureEmitAssets: true,
+    chunkFilename: "static/js/[name].[contenthash:8].chunk.js",
   },
   optimization: {
-		minimize: true,
-		minimizer: [
-			new TerserPlugin({
-				terserOptions: {
-					parse: {
-						ecma: 8,
-					},
-					compress: {
-						ecma: 5,
-						warnings: false,
-						comparisons: false,
-						inline: 2,
-					},
-					mangle: {
-						safari10: true,
-					},
-					output: {
-						ecma: 5,
-						comments: false,
-						ascii_only: true,
-					},
-				},
-				extractComments: false,
-				parallel: true,
-				cache: true,
-				sourceMap: true,
-			}),
-		],
-		splitChunks: {
-			chunks: "all",
-			name: false,
-		},
-		runtimeChunk: {
-			name: entrypoint => `runtime-${entrypoint.name}`,
-		},
-	},
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          parse: {
+            ecma: 8,
+          },
+          compress: {
+            ecma: 5,
+            warnings: false,
+            comparisons: false,
+            inline: 2,
+          },
+          mangle: {
+            safari10: true,
+          },
+          output: {
+            ecma: 5,
+            comments: false,
+            ascii_only: true,
+          },
+        },
+        extractComments: false,
+        parallel: true,
+        cache: true,
+        sourceMap: true,
+      }),
+    ],
+    splitChunks: {
+      chunks: "all",
+      name: false,
+    },
+    runtimeChunk: {
+      name: entrypoint => `runtime-${entrypoint.name}`,
+    },
+  },
   resolve: {
-		modules: [
-			paths.appSrc,
-			"node_modules",
-		],
+    modules: [
+      paths.appSrc,
+      "node_modules",
+    ],
     extensions: [
       ".js",
       ".jsx",
@@ -70,91 +70,91 @@ const config = {
   },
   module: {
     rules: [
-			{
-				oneOf: [
-					{
-						test: /\.(js|jsx)$/,
-						use: "babel-loader",
-						exclude: /node_modules/
-					},
-					{
-						test: /\.(ts|tsx)?$/,
-						loader: "ts-loader",
-						exclude: /node_modules/
-					},
-					{
-						test: /\.scss$/,
-						use: [
-							{
-								loader: MiniCssExtractPlugin.loader,
-								options: {},
-							},
-							{
-								loader: "css-loader",
-								options: {
-									importLoaders: 2,
-									sourceMap: false,
-									modules: true,
-								},
-							},
-							{
-								loader: "sass-loader",
-								options: {},
-							}
-						]
-					},
-					{
-						test: /\.png$/,
-						use: [
-							{
-								loader: "url-loader",
-								options: {
-									mimetype: "image/png"
-								}
-							}
-						]
-					},
-					{
-						loader: "file-loader",
-						exclude: [/\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/],
-						options: {
-							name: "static/media/[name].[hash:8].[ext]",
-						},
-					},
-				]
-			},
-		]
-	},
-	plugins: [
-		new CleanWebpackPlugin(),
-		new CopyWebpackPlugin([
-			{ from: "public" },
-		]),
-		...Object.keys(pages.entries).map(entryName =>
-			new HtmlWebpackPlugin({
-				inject: true,
-				chunks: [entryName],
-				filename: `${entryName}.html`,
-				template: `./templates/${entryName}.html`,
-				favicon: "./public/favicon.ico",
-				minify: {
-					removeComments: true,
-					collapseWhitespace: true,
-					removeRedundantAttributes: true,
-					useShortDoctype: true,
-					removeEmptyAttributes: true,
-					removeStyleLinkTypeAttributes: true,
-					keepClosingSlash: true,
-					minifyJS: true,
-					minifyCSS: true,
-					minifyURLs: true,
-				},
-			})
-		),
-		new MiniCssExtractPlugin({
-			filename: "static/css/[name].[contenthash:8].css",
-			chunkFilename: "static/css/[name].[contenthash:8].chunk.css",
-		}),
+      {
+        oneOf: [
+          {
+            test: /\.(js|jsx)$/,
+            use: "babel-loader",
+            exclude: /node_modules/
+          },
+          {
+            test: /\.(ts|tsx)?$/,
+            loader: "ts-loader",
+            exclude: /node_modules/
+          },
+          {
+            test: /\.scss$/,
+            use: [
+              {
+                loader: MiniCssExtractPlugin.loader,
+                options: {},
+              },
+              {
+                loader: "css-loader",
+                options: {
+                  importLoaders: 2,
+                  sourceMap: false,
+                  modules: true,
+                },
+              },
+              {
+                loader: "sass-loader",
+                options: {},
+              }
+            ]
+          },
+          {
+            test: /\.png$/,
+            use: [
+              {
+                loader: "url-loader",
+                options: {
+                  mimetype: "image/png"
+                }
+              }
+            ]
+          },
+          {
+            loader: "file-loader",
+            exclude: [/\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/],
+            options: {
+              name: "static/media/[name].[hash:8].[ext]",
+            },
+          },
+        ]
+      },
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyWebpackPlugin([
+      { from: "public" },
+    ]),
+    ...Object.keys(pages.entries).map(entryName =>
+      new HtmlWebpackPlugin({
+        inject: true,
+        chunks: [entryName],
+        filename: `${entryName}.html`,
+        template: `./templates/${entryName}.html`,
+        favicon: "./public/favicon.ico",
+        minify: {
+          removeComments: true,
+          collapseWhitespace: true,
+          removeRedundantAttributes: true,
+          useShortDoctype: true,
+          removeEmptyAttributes: true,
+          removeStyleLinkTypeAttributes: true,
+          keepClosingSlash: true,
+          minifyJS: true,
+          minifyCSS: true,
+          minifyURLs: true,
+        },
+      })
+    ),
+    new MiniCssExtractPlugin({
+      filename: "static/css/[name].[contenthash:8].css",
+      chunkFilename: "static/css/[name].[contenthash:8].chunk.css",
+    }),
   ],
 }
 
